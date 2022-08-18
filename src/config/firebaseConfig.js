57eea029b9a9abf4c1d03631f6d99d0a21714563
INDRANIL SAMANTA <you@example.com>
2022-08-18 19:04:12 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCPbERuDqn7Confmtcii-Q0yldIaixf2q0",
+  authDomain: "crypto-dashboard-a7468.firebaseapp.com",
+  projectId: "crypto-dashboard-a7468",
+  storageBucket: "crypto-dashboard-a7468.appspot.com",
+  messagingSenderId: "125938621311",
+  appId: "1:125938621311:web:ffd35da923ce11d730584a"
+};
+
+export default firebaseConfig;
